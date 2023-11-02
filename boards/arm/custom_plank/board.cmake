0edb17a107e7df985b63fd558207a427e06cd035
pengyang qiu <pyqiu@linkedsemi.com>
2023-11-02 14:11:44 +0800

    set custom plank to le501x_evb

--- boards/arm/custom_plank/board.cmake
+++ boards/arm/custom_plank/board.cmake
@@ -1,12 +1,6 @@
-# Copyright (c) 2021 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
-board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+set_property(TARGET runners_yaml_props_target PROPERTY hex_file "production.hex")
 
-set(OPENOCD_NRF5_SUBFAMILY "nrf52")
-
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+board_runner_args(jlink "--device=LE501X" "--speed=4000" "--erase" "--reset-after-load")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
